778e60e87a03f8eea3e8a3faae9308ffaa35f13d
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-02 19:47:32 +0200

    microphone capture test and also that getusermedia fucntion I uesd before is apparently deprecated now

--- Media.js
+++ Media.js
@@ -1,4 +1,36 @@
 
+// navigator.getUserMedia =
+//   navigator.getUserMedia ||
+//   navigator.webkitGetUserMedia ||
+//   navigator.mozGetUserMedia;
+
+// if (navigator.getUserMedia) {
+//   navigator.getUserMedia(
+//     { audio: true, video: { width: 1280, height: 720 } },
+//     (stream) => {
+//       const video = document.querySelector("video");
+//       video.srcObject = stream;
+//       video.onloadedmetadata = (e) => {
+//         video.play();
+//       };
+//     },
+//     (err) => {
+//       console.error(`The following error occurred: ${err.name}`);
+//     },
+//   );
+// } else {
+//   console.log("getUserMedia not supported");
+// }
+
 function Media() {
-	
+	async function getMicrophoneStream() {
+		try {
+			const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+			
+			console.log("Microphone stream active:", stream);
+			return stream;
+		} catch (error) {
+			console.error("Microphone access denied or failed:", error);
+		}
+	}
 }
